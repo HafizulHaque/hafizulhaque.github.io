0a3761ce1ce72c457a8db6247b65c4fbc7bdc171
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2023-12-20 15:53:53 +0600

    guestbook feature deleted

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   const navItems = [
     { label: 'Home', path: '/'},
-    { label: 'GuestBook', path: '/guestbook'},
+    // { label: 'GuestBook', path: '/guestbook'},
     { label: 'Projects', path: '/projects'},
   ]
 
